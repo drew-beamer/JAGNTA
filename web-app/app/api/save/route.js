1e8b748f53c9d99048639804f2efb93e06f02eaa
drew-beamer <drbeamer@davidson.edu>
2023-12-01 21:48:21 -0500

    working api and utils

--- web-app/app/api/save/route.js
+++ web-app/app/api/save/route.js
@@ -1,4 +1,5 @@
 import connection from "@/lib/db";
+import { getUniqueNonStopwords } from "@/lib/utils/nlp/stopword";
 import { NextRequest, NextResponse } from "next/server";
 
 /**
@@ -7,8 +8,24 @@ import { NextRequest, NextResponse } from "next/server";
  */
 export async function POST(req) {
   const { content, id } = await req.json();
+
+  const uniqueNonStopwords = getUniqueNonStopwords(content);
+
+  const loadedConnection = await connection;
+
+  await loadedConnection.query("START TRANSACTION");
+  await loadedConnection.query("DELETE FROM NotesIndex WHERE id = ?", [id]);
+
+  uniqueNonStopwords.forEach(
+    async (stopword) =>
+      await loadedConnection.query("INSERT IGNORE INTO NotesIndex VALUES (?, ?)", [
+        id,
+        stopword,
+      ])
+  )
   await (
     await connection
-  ).query("UPDATE Notes SET content = ? WHERE id = ?;", [content, id]);
+    ).query("UPDATE Notes SET content = ? WHERE id = ?;", [content, id]);
+  await loadedConnection.query("COMMIT");
   return NextResponse.json({ message: "success" });
 }
