83b3e547cee3d7c4c5e2982c6887eadcefdaed9c
drew-beamer <drbeamer@davidson.edu>
2023-12-06 11:05:31 -0500

    add page, form

---
+++ web-app/app/advanced-search/form.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { Input } from "@nextui-org/react";
+
+export default function AdvancedSearchForm() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
---
+++ web-app/app/advanced-search/page.js
@@ -0,0 +1,11 @@
+
+
+/**
+ * Responsible for rendering the Advanced Search Page
+ * @returns a RSC containing the page contents
+ */
+export default function AdvancedSearchPage() {
+    return <main>
+
+    </main>
+}
