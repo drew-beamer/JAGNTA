1702c6f07ccb23aa167ea7446360e4716c23847a
drew-beamer <drbeamer@davidson.edu>
2023-12-11 22:21:53 -0500

    implement functional advanced search

--- web-app/app/api/search/route.js
+++ web-app/app/api/search/route.js
@@ -1,26 +1,48 @@
 import { NextRequest, NextResponse } from "next/server";
-import connection from '@/lib/db'
+import connection from "@/lib/db";
+
+function expandQuery(currentQuery, operation) {
+  return `(${currentQuery} ${
+    operation === "OR" ? "UNION" : operation === "AND" ? "INTERSECT" : "EXCEPT"
+  } SELECT id FROM NotesIndex WHERE word = ?)`;
+}
+
+function joinQuery(filteredNoteTableQuery) {
+  return `SELECT id, title, content FROM Notes JOIN (SELECT id, count(id) as main_count FROM NotesIndex WHERE id IN ${filteredNoteTableQuery} AND word = ? GROUP BY id) as doc_counts USING(id) ORDER BY main_count DESC`;
+}
 
 /**
- * 
- * @param {NextRequest} req 
+ *
+ * @param {NextRequest} req
  */
 export async function GET(req) {
-    const params = req.nextUrl.searchParams;
-    console.log(params);
-    // Main thing to search--rest define articles to include/not include
-    /*
+  const params = req.nextUrl.searchParams;
+  const paramArray = Array.from(params.entries());
+  if (paramArray.length % 2 !== 1) {
+    return NextResponse.error();
+  }
+  // Main thing to search--rest define articles to include/not include
+  /*
     'Pizza' - gets articles mentioning pizza
     'Pizza and pepperoni' gets articles mentioning pizza and pepperoni
     'Pizza and pepperoni not cheese' gets articles mentioning pizza and pepperoni, but not cheese
     Essentially l -> r evaluation with no parentheses
     'Pizza not cheese and pepperoni' has the same evaluation as the former
     */
-    const main = params.get('main');
-    console.log(params)
-    const loadedConnection = await connection;
-    const mainDocuments = await loadedConnection.query("SELECT id, COUNT(id) FROM NotesIndex WHERE word = ? GROUP BY id", [main]);
-    console.log(mainDocuments)
-    return NextResponse.json({message: "success!"})
+  const main = params.get("main");
+  const loadedConnection = await connection;
+  const baseQuery = `(SELECT id FROM NotesIndex WHERE word = ?)`;
+  const queryParams = [main];
+  let currentQuery = baseQuery;
+  for (let i = 0; i < Math.floor(paramArray.length / 2); i++) {
+    const operator = params.get(`entry${i}-select`);
+    const word = params.get(`entry${i}-search`);
+    currentQuery = expandQuery(currentQuery, operator);
+    queryParams.push(word);
+  }
+  currentQuery = joinQuery(currentQuery);
+  queryParams.push(main);
+  const res = (await loadedConnection.query(currentQuery, queryParams))[0];
 
+  return NextResponse.json(res);
 }
